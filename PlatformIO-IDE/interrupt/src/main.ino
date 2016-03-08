936441b813a32268ef697f4bcd8f2679520cef6f
Nelis Willers <neliswillers@gmail.com>
2016-03-08 21:52:16 +0200

    interim commit - trying to get mqtt going on esp8266 interrupt

--- PlatformIO-IDE/interrupt/src/main.ino
+++ PlatformIO-IDE/interrupt/src/main.ino
@@ -3,8 +3,68 @@
 // SwitchDoc Labs  October 2015
 //
 
+
+//install PubSubClient from http://platformio.org/#!/lib
+//download the tar file and untar into the project lib folder
+
+
+//mosquitto_sub -v -d -t "testing/photocell"
+
 extern "C" {
 #include "user_interface.h"
+}
+
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#define wifi_ssid "CJMSW"
+#define wifi_password "00300ABE3FE8"
+
+#define mqtt_server "10.0.0.16"
+#define mqtt_user ""
+#define mqtt_password ""
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    if (client.connect("ESP8266Client")) {
+    ///if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 os_timer_t myTimer;
@@ -63,6 +123,10 @@ void setup() {
  Serial.println("--------------------------");
  Serial.println("ESP8266 Timer Test");
  Serial.println("--------------------------");
+
+ setup_wifi();
+ client.setServer(mqtt_server, 1883);
+
  tickOccured = false;
  user_init();
 }
@@ -71,6 +135,7 @@ void loop() {
 
  if (tickOccured == true)
  {
+    client.publish("testing/photocell", "yes", true);
     Serial.println("Tick Occurred");
     tickOccured = false;
  }
