d9ee6d7a41aeae6d56d913972a83dc50a61ec90e
Nelis Willers <neliswillers@gmail.com>
2016-03-10 19:07:04 +0200

    first version where mqtt is working

--- PlatformIO-IDE/interrupt/src/main.ino
+++ PlatformIO-IDE/interrupt/src/main.ino
@@ -9,6 +9,7 @@
 
 
 //mosquitto_sub -v -d -t "testing/mqtttest"
+//mosquitto_sub -v -d -t "testing/+"
 
 extern "C" {
 #include "user_interface.h"
@@ -16,6 +17,14 @@ extern "C" {
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+
+//#include "DHT.h"
+//#define DHTPIN 13     // what digital pin we're connected to
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//DHT dht(DHTPIN, DHTTYPE);
 
 #include "../../../../openHABsysfiles/password.h"
 //#define wifi_ssid "yourwifiSSID"
@@ -26,6 +35,10 @@ extern "C" {
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+int counter = 0;
+int previousReading = LOW;
+os_timer_t myTimer;
+bool tickOccured;
 
 void setup_wifi() {
   delay(10);
@@ -52,6 +65,10 @@ void reconnect() {
     if (client.connect("ESP8266Client")) {
     ///if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
       Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("testing/mqtttest", "hello world");
+      // ... and resubscribe
+      client.subscribe("testing/espinput");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -62,58 +79,48 @@ void reconnect() {
   }
 }
 
-os_timer_t myTimer;
-
-bool tickOccured;
 
 // start of timerCallback
 void timerCallback(void *pArg)
 {
       tickOccured = true;
-} // End of timerCallback
+}
 
 void user_init(void)
 {
-//   os_timer_setfn - Define a function to be called when the timer fires
-//
-// void os_timer_setfn(
-//       os_timer_t *pTimer,
-//       os_timer_func_t *pFunction,
-//       void *pArg)
-//
-// Define the callback function that will be called when the timer reaches zero. The pTimer parameters is a pointer to the timer control structure.
-//
-// The pFunction parameters is a pointer to the callback function.
-//
-// The pArg parameter is a value that will be passed into the called back function. The callback function should have the signature:
-// void (*functionName)(void *pArg)
-//
-// The pArg parameter is the value registered with the callback function.
+// Initialize the BUILTIN_LED pin as an output
+  pinMode(BUILTIN_LED, OUTPUT);
+  //PIR is on GPIO12, or D6 on the NodeMCU
+  pinMode(12, INPUT);
 
-      os_timer_setfn(&myTimer, timerCallback, NULL);
-
-//       os_timer_arm -  Enable a millisecond granularity timer.
-//
-// void os_timer_arm(
-//       os_timer_t *pTimer,
-//       uint32_t milliseconds,
-//       bool repeat)
-//
-// Arm a timer such that is starts ticking and fires when the clock reaches zero.
-//
-// The pTimer parameter is a pointed to a timer control structure.
-// The milliseconds parameter is the duration of the timer measured in milliseconds. The repeat parameter is whether or not the timer will restart once it has reached zero.
-
-      os_timer_arm(&myTimer, 1000, true);
+    //Define a function to be called when the timer fires
+    os_timer_setfn(&myTimer, timerCallback, NULL);
+    // Enable a millisecond granularity timer.
+    os_timer_arm(&myTimer, 1000, true);
 }
 
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
 
 void setup() {
 
  Serial.begin(115200);
- Serial.println();
- Serial.println();
-
  Serial.println("");
  Serial.println("--------------------------");
  Serial.println("ESP8266 Timer Test");
@@ -121,16 +128,55 @@ void setup() {
 
  setup_wifi();
  client.setServer(mqtt_server, 1883);
+ client.setCallback(mqttCallback);
 
  tickOccured = false;
  user_init();
+
+ //dht.begin();
+
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  /*long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    // Read humidity and temperature as Celsius (the default)
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+    char t2[50];
+    char h2[50];
+    dtostrf(t, 5, 2, t2);
+    dtostrf(h, 5, 2, h2);
+    client.publish("/sensor/temp", t2 );
+    client.publish("/sensor/humidity", h2 );
+  }*/
+
+  int reading = digitalRead(12);
+  Serial.println(reading);
+  if (previousReading == LOW && reading == HIGH) {
+  counter++;
+  client.publish("testing/movement", "Motion!");
+  previousReading = reading;
+  //Serial.print("PIR ");
+  //Serial.print(counter);
+  //Serial.print("x Times! ");
+}
 
  if (tickOccured == true)
  {
-    if (! client.publish("testing/mqtttest", "yes"))
+    if (! client.publish("testing/mqtttest", "yes!! Wow!!"))
     {
       Serial.println("Publish failed");
     };
